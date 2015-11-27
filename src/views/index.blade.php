8c9abf3c370f8e8af82017373dd7119af4332c5f
Matt <matt@redtrainwebservices.com>
2015-11-27 11:10:21 -0700

    removed form facade dependency

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <h1>Import CSV Content</h1>
     <div class="imports-index">
-        {!!Form::open(['files'=>'true','class' => 'form-horizontal'])!!}
+        <form action="" accept-charset="UTF-8" class="form-horizontal" enctype="multipart/form-data">
         @foreach($fields as $field)
             <fieldset class="form-group">
                 {!! Form::label($field,ucwords(str_replace('_',' ',$field)).' CSV:',['class'=>'control-label col-sm-2']) !!}
@@ -14,6 +14,6 @@
             </fieldset>
         @endforeach
         <button type="submit" class="btn btn-success" onclick="confirm('Are You Sure?')">Submit</button>
-        {!!Form::close()!!}
+        </form>
     </div>
 @stop
