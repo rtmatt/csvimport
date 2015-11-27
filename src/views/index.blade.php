e04b118370f3d7144c4f75331e70a865c59d9aed
Matt <matt@redtrainwebservices.com>
2015-11-27 12:06:04 -0700

    removed form facade dependency completely

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -2,18 +2,21 @@
 @section('content')
     <h1>Import CSV Content</h1>
     <div class="imports-index">
-        <form action="" accept-charset="UTF-8" class="form-horizontal" enctype="multipart/form-data">
-        @foreach($fields as $field)
-            <fieldset class="form-group">
-                {!! Form::label($field,ucwords(str_replace('_',' ',$field)).' CSV:',['class'=>'control-label col-sm-2']) !!}
-                <div class="col-sm-10">
-                    <div class="form-control-static">
-                        {!! Form::file($field,null,['class'=>'form-control'])!!}
+        <form action="" method="POST" accept-charset="UTF-8" class="form-horizontal" enctype="multipart/form-data">
+            <input type="hidden" name="_token" value={{csrf_token()}}>
+            @foreach($fields as $field)
+                <fieldset class="form-group">
+                    <label for="{{$field}}" class="control-label col-sm-2">
+                        {{ucwords(str_replace('_',' ',$field)).' CSV:'}}
+                    </label>
+                    <div class="col-sm-10">
+                        <div class="form-control-static">
+                            <input type="file" name="{{$field}}"/>
+                        </div>
                     </div>
-                </div>
-            </fieldset>
-        @endforeach
-        <button type="submit" class="btn btn-success" onclick="confirm('Are You Sure?')">Submit</button>
+                </fieldset>
+            @endforeach
+        <button type="submit" class="btn btn-success">Submit</button>
         </form>
     </div>
 @stop
