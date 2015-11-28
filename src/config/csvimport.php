40b31185d9776a0bfb5beb1dc737d01d5f531ae9
Matt <matt@redtrainwebservices.com>
2015-11-28 14:07:25 -0700

    added new configs

--- src/config/csvimport.php
+++ src/config/csvimport.php
@@ -1,7 +1,10 @@
 <?php
-
+//@todo: Add concrete importer config directry
 return [
     'import_order'=>[],
     'auth'=>false,
-    'sql_directory'=>'/data'
+    'sql_directory'=>'/data',
+    'override_layout_view'=> false,
+    'importer_namespace'=>"\\App\\CSVImports\\",
+    'importer_directory'=>app_path('CSVImports')
 ];
