958c12d0e4c03bc9a166f5ea75dceede5bc2a430
Matt <matt@redtrainwebservices.com>
2015-11-28 14:04:11 -0700

    package exceptions

---
+++ src/Exceptions/CSVImportInvalidLayoutException.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mattemrick
+ * Date: 11/28/15
+ * Time: 12:25 PM
+ */
+
+namespace RTMatt\CSVImport\Exceptions;
+
+class CSVImportInvalidLayoutException extends \Exception{}
