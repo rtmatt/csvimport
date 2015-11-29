4d0ba448faf8a92601e1ef38a8086cfcd65dad0f
Matt <matt@redtrainwebservices.com>
2015-11-29 09:57:59 -0700

    incomplete importer exception

---
+++ src/Exceptions/CSVIncompleteImporterException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mattemrick
+ * Date: 11/29/15
+ * Time: 9:55 AM
+ */
+
+namespace RTMatt\CSVImport\Exceptions;
+
+
+class CSVIncompleteImporterException extends \Exception{}
--- src/views/importer_stub.blade.php
+++ src/views/importer_stub.blade.php
@@ -8,17 +8,20 @@ class {{studly_case($importer_name)}}Importer extends CSVImporter {
     protected function setResourceName()
     {
         //TODO: Implement this method
+        throw new \RTMatt\CSVImport\Exceptions\CSVIncompleteImporterException("setResourceName method needs to be implemented");
     }
 
     protected function setTableName()
     {
-        //TODO: Implement this method
+         //TODO: Implement this method
+        throw new \RTMatt\CSVImport\Exceptions\CSVIncompleteImporterException("setTableName method needs to be implemented");
     }
 
 
     protected function setFieldString()
     {
-        //TODO: Implement this method
+         //TODO: Implement this method
+        throw new \RTMatt\CSVImport\Exceptions\CSVIncompleteImporterException("setFieldString method needs to be implemented");
     }
 
 
